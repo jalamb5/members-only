d00c3a948ad266c3ce25f1d8b45cdb0483060200
Justin Lamb <justinlamb@me.com>
2022-10-16 13:01:10 -0700

    basic view display

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "home#index"
+  root "posts#index"
 
   resources :posts, only: [:new, :create, :index]
 end
