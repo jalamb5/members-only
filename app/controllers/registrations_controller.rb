61cbfbf6d9020f968978b686a8fae32dacbb69ff
Justin Lamb <justinlamb@me.com>
2022-10-17 13:06:40 -0700

    add error checking

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,8 @@
-class UserController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
+  private
+  def sign_up_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
   # def new
   #   @user = User.new
   # end
